2186c844d0222359b79424aab817a341a3d052c5
Patrik Boloz <35575791+patrikboloz@users.noreply.github.com>
2018-04-02 15:01:27 -0600

    Add files via upload
    Modified files for homework

---
+++ views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+def index(request):
+    template = loader.get_template('homepage.html') 
+    return HttpResponse(template.render())
